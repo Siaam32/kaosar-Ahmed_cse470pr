53471c4af2f40164735571e3c319b4ca6303e185
unknown <you@example.com>
2022-08-21 00:17:31 +0600

    first message

---
+++ view/src/App.js
@@ -0,0 +1,42 @@
+
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import About from './components/About/About';
+import Header from './components/Header/Header';
+import Inventory from './components/Help/Help';
+import Login from './components/Login/Login';
+import Orders from './Model/Orders';
+import RequireAuth from './components/RequireAuth/RequireAuth';
+import Shipment from './components/Shipment/Shipment';
+import Shop from './components/Shop/Shop';
+import Signup from './components/Signup/Signup';
+import Help from './components/Help/Help';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Shop></Shop>}></Route>
+        <Route path='/shop' element={<Shop></Shop>}></Route>
+        <Route path='/orders' element={<Orders></Orders>}></Route>
+        <Route path='/help' element={
+          <RequireAuth>
+            <Help></Help>
+          </RequireAuth>
+        }></Route>
+        <Route path="/shipment" element={
+          <RequireAuth>
+            <Shipment></Shipment>
+          </RequireAuth>
+        }></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/signup' element={<Signup></Signup>}></Route>
+        
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
